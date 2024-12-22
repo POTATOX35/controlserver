876795aace8c17b3950eba844e485596bc7cf94e
POTATOX <mustafakr83@gmail.com>
2024-12-22 11:51:58 +0300

    main

---
+++ main.js
@@ -0,0 +1,25 @@
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+const PORT = 3000;
+app.use(cors({
+    origin: '*',  // Geliştirme ortamında her yerden gelen istekleri kabul edebiliriz
+    methods: ['GET', 'POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+app.post('/api/command', (req, res) => {
+    const command = req.body.command;
+    console.log(`Received command: ${command}`);
+
+    // Gelen komut üzerinde işlem yap
+    if (command === 'execute_task') {
+        // İstemciye bir mesaj gönderin veya işlem yapın
+        res.status(200).send('Task executed');
+    } else {
+        res.status(400).send('Unknown command');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server ${PORT} portunda çalışıyor.`);
+});
